b509f69682f6508cedfac96416aa1c80f97c651a
Fiicus <2471346734@qq.com>
2024-01-07 00:55:07 +0800

    修改kmeans后缀为.py

--- kmeans.py
+++ kmeans.py
@@ -13,13 +13,12 @@ def cluster():
     text_topic_matrix, topic_word_matrix, words = lda.train_lda_model(data, num_topics=num_topic)
     chi = np.empty((num_cluster, num_topic))
     for i in range(len(text_topic_matrix)):
-        if(len(text_topic_matrix[i])<15):
+        if len(text_topic_matrix[i]) < 15:
             dimensions_to_pad = num_topic - len(text_topic_matrix[i])
             padding = (0, dimensions_to_pad)
             text_topic_matrix[i] = np.pad(text_topic_matrix[i], padding, mode='constant', constant_values=0)
 
-
-    #为了画图
+    # 为了画图
     pca = PCA(n_components=2)
     X_pca = pca.fit_transform(text_topic_matrix)
     kmeans = KMeans(n_clusters=num_cluster)
@@ -33,7 +32,7 @@ def cluster():
     plt.ylabel('Feature 2')
     plt.show()
 
-    #为了实验效果
+    # 为了实验效果
     kmeans = KMeans(n_clusters=num_cluster)
     kmeans.fit(text_topic_matrix)
     labels = kmeans.labels_
@@ -50,16 +49,16 @@ def cluster():
         m.extend(np.vstack(clusters[i]).tolist())
     m = np.array(m)
 
-    n = len(m)                              #文档数
+    n = len(m)  # 文档数
     print(n)
     print(len(text_topic_matrix))
     for i in range(num_cluster):
-        mi = np.vstack(clusters[i])         #第i类的所有文档主题关系矩阵
+        mi = np.vstack(clusters[i])  # 第i类的所有文档主题关系矩阵
         for j in range(num_topic):
-            a = np.sum(mi[:, j])            #第i类中第j个主题的数目
-            b = len(mi) - a                 #第i类中没有第j个主题的数目
-            c = np.sum(m[:, j]) - a         #除第i类外包含第j个主题的文档数
-            d = n - len(mi) - c             #除第i类外不包含第j个主题的文档数
+            a = np.sum(mi[:, j])  # 第i类中第j个主题的数目
+            b = len(mi) - a  # 第i类中没有第j个主题的数目
+            c = np.sum(m[:, j]) - a  # 除第i类外包含第j个主题的文档数
+            d = n - len(mi) - c  # 除第i类外不包含第j个主题的文档数
             h = n * (a * d - b * c) ** 2
             k = (a + c) * (b + d) * (a + b) * (c + d)
             chi[i][j] = h / k
@@ -76,11 +75,13 @@ def cluster():
             word_prob[words[sorted_chi[j]]] = res[i][sorted_chi[j]]
         print(word_prob)
         mask = imageio.imread(r'bbll.jpeg')
-        wordcloud = WordCloud(width=800, height=400, background_color='white', font_path='ZhiMangXing-Regular.ttf').generate_from_frequencies(word_prob)
+        wordcloud = WordCloud(width=800, height=400, background_color='white',
+                              font_path='ZhiMangXing-Regular.ttf').generate_from_frequencies(word_prob)
         plt.figure(figsize=(10, 5))
         plt.imshow(wordcloud, interpolation='bilinear')
         plt.axis('off')
         plt.show()
+
 
 if __name__ == '__main__':
     cluster()
